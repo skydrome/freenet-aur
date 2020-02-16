a6af2371aa828288cefbe21db9f617d8f15fcdca
skydrome <skydrome@protonmail.org>
2020-02-16 12:57:19 -0500

    god

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,9 @@ sudo chown -R makepkg /build /github/workspace
 
 cd /build
 
-export GNUPGHOME=/build/.gnupg
-gpg --recv-keys FD6C57F9
-
 git clone --depth 1 "https://aur.archlinux.org/freenet.git" .
 sed -i "s|'java-service-wrapper'||" PKGBUILD
 
+sudo gpg --recv-keys FD6C57F9
 PKGDEST="/github/workspace" \
     makepkg -si --noconfirm
